d0371267598864b774619e2567ad4c7955ab0fa9
JuandiJ7 <jdiego.jacques@correo.ucu.edu.uy>
2022-09-11 00:42:41 -0300

    Ejercicio 1

--- Ejercicio 1/src/Library/Libro.cs
+++ Ejercicio 1/src/Library/Libro.cs
@@ -2,14 +2,16 @@
 
 namespace SRP
 {
-    public class Book
+    public class Book /*La clase no cumple con el 
+    principio SRP ya que no solo tiene una responsabilidad sobre el libro 
+    (conocer su título, su autor y su código), sino que también tiene una 
+    responsabilidad sobre la ubicación del libro. Si se cambia la ubicación
+    del libro, se debe cambiar la clase.*/
     {
 
         public string Title { get ; }
         public string Author { get ; }
         public string Code { get ;  }
-        public string LibrarySector { get ; set; }
-        public string LibraryShelve { get ; set; }
 
         public Book(String title, String author, String code)
         {
@@ -17,12 +19,17 @@ namespace SRP
             this.Author = author;
             this.Code = code;
         }
-
-        public void ShelveBook(String sector, String shelve)
-        {
-            this.LibrarySector = sector;
-            this.LibraryShelve = shelve;
-        }
-
     }
+
+    public class BookLocation //De esta forma, cada clase tiene su única razón por la cual cambiar.
+    {
+        public string LibrarySector { get ; set; }
+        public string LibraryShelve { get ; set; }
+        public void ShelveBook(String sector, String shelve)
+            {
+                this.LibrarySector = sector;
+                this.LibraryShelve = shelve;
+            }
+    }
+        
 }
